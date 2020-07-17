3996dc6ccd457cd639fd0b59ae48f0642a63da93
Alex Nugent <aon@Alexs-MacBook-Air.local>
2020-07-17 16:37:23 -0500

    add WeatherService test file

---
+++ src/__test__/WeatherService.test.js
@@ -0,0 +1,19 @@
+import requestWeatherData from '../services/WeatherService';
+import axios from 'axios';
+
+describe('WeatherService tests', () => {
+      
+    it('Api call success', async () => {
+        const expectedData = {
+            data: {
+                current: {},
+                forecast: {}
+            }
+        }
+        axios.get = jest.fn(() => Promise.resolve({data: { current: {}, forecast: {} } }));
+        let returnedData = await requestWeatherData({ units: "units", zipcode: "zip" });
+        expect(returnedData).toEqual(expectedData);
+        expect(axios.get).toHaveBeenCalledTimes(1);
+        axios.get.mockClear();
+    });
+})
