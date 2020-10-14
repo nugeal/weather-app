c362688f96404a61a0c6312798af20b7b82bb280
Alex Nugent <aon@Alexs-MacBook-Air.local>
2020-10-13 22:11:01 -0500

    add searchForm test file

--- src/__test__/SearchForm.test.js
+++ src/__test__/SearchForm.test.js
@@ -6,29 +6,36 @@ let wrapper;
 
 describe('SearchForm tests', () => {
     beforeEach(() => {
-        wrapper = mount(<SearchForm />);
-      });
+        wrapper = mount(<SearchForm getWeatherData={ () => {} }/>);
+    });
       
-      it('zipcode input field should handle text entry', () => {
-          wrapper.find('Input').childAt(0).childAt(0).simulate('change', { target: { name: 'zipcode', value: '90210' } });
-          expect(wrapper.find('Input').childAt(0).childAt(0).prop('value')).toEqual('90210');
-      });
-      
-      it('units input field value should default to imperial', () => {
-          expect(wrapper.find('Select').childAt(0).prop('value')).toEqual('imperial');
-      });
-      
-      it('units input field should handle option change to metric', () => {
-          wrapper.find('div#unitsField').childAt(2).childAt(0).childAt(1).simulate('click');
-          expect(wrapper.find('Select').prop('value')).toEqual('metric');
-      });
-      
-      it('Get Weather button should be disabled by default', () => {
-          expect(wrapper.find('Button').prop('disabled')).toBe(true);
-      });
-      
-      it('Get Weather button should be enabled when a valid zipcode is entered', () => {
-          wrapper.find('Input').childAt(0).childAt(0).simulate('change', { target: { name: 'zipcode', value: '90210' } });
-          expect(wrapper.find('Button').prop('disabled')).toBe(false);
-      });
+    it('zipcode input field should handle text entry', () => {
+        wrapper.find('Input').childAt(0).childAt(0).simulate('change', { target: { name: 'zipcode', value: '90210' } });
+        expect(wrapper.find('Input').childAt(0).childAt(0).prop('value')).toEqual('90210');
+    });
+    
+    it('units input field value should default to imperial', () => {
+        expect(wrapper.find('Select').childAt(0).prop('value')).toEqual('imperial');
+    });
+    
+    it('units input field should handle option change to metric', () => {
+        wrapper.find('div#unitsField').childAt(2).childAt(0).childAt(1).simulate('click');
+        expect(wrapper.find('Select').prop('value')).toEqual('metric');
+    });
+    
+    it('Get Weather button should be disabled by default', () => {
+        expect(wrapper.find('Button').prop('disabled')).toBe(true);
+    });
+    
+    it('Get Weather button should be enabled when a valid zipcode is entered', () => {
+        wrapper.find('Input').childAt(0).childAt(0).simulate('change', { target: { name: 'zipcode', value: '90210' } });
+        expect(wrapper.find('Button').prop('disabled')).toBe(false);
+        wrapper.find('Form').childAt(0).simulate('submit');
+    });
+
+    it('Zipcode text field should be in an error state when an invalid zipcode is entered', () => {
+        wrapper.find('Input').childAt(0).childAt(0).simulate('change', { target: { name: 'zipcode', value: '902' } });
+        expect(wrapper.find('Button').prop('disabled')).toBe(true);
+        expect(wrapper.find('FormField').at(0).prop('error')).toBe(true);
+    });
 })
